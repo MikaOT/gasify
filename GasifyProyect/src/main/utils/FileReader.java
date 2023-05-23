bf989b8c3668796cbdc1b6a1fffd8b4cd3f16080
CarlosCasadoo <carlos.casado@alumnos.uneatlantico.es>
2023-05-23 09:24:37 +0200

    feature: added the file reader utility

--- GasifyProyect/src/main/utils/FileReader.java
+++ GasifyProyect/src/main/utils/FileReader.java
@@ -1,10 +1,50 @@
 package main.utils;
 
-public class FileReader { // Clase para leer
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class FileReader {
 
+    private List<String> lines;
 
+    public FileReader(){
+        this.lines = new ArrayList<>();
+    }
 
+    private List<String> readFileContent(String path)  {
+        BufferedReader input = null;
 
+        try {
+            input = new BufferedReader(new java.io.FileReader(path));
+            String line;
+            while ((line = input.readLine()) != null) {
+                this.lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            //TO-DO EXCEPTIONS
+        } catch (IOException e) {
+            //TO-DO EXCEPTIONS
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    //TO-DO EXCEPTIONS
+                }
+            }
+        }
+
+        return this.lines;
+
+    }
+
+    public List<String> loadFileContent(String path) {
+        File file = new File(path);
+        return readFileContent(path);
+    }
 
 }
